ee8cb6dbaf115b5c2180e08a39cb16650d3a7e54
colinSeven8 <ccnorse80@gmail.com>
2020-01-14 21:48:28 -0700

    Updates

---
+++ precache-manifest.960ac7028fa1eeba54b67968ba3489c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c39964b6ae39a945dba759d976e3537d",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "b7f4e18b58920df18f74",
+    "url": "/Clicky-Game/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b6191dbe957818e12a89",
+    "url": "/Clicky-Game/static/css/main.2dd672e1.chunk.css"
+  },
+  {
+    "revision": "b7f4e18b58920df18f74",
+    "url": "/Clicky-Game/static/js/2.4b5048b0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.4b5048b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6191dbe957818e12a89",
+    "url": "/Clicky-Game/static/js/main.024f246d.chunk.js"
+  },
+  {
+    "revision": "84e32ec22331b830aa1a",
+    "url": "/Clicky-Game/static/js/runtime-main.628f24fa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
